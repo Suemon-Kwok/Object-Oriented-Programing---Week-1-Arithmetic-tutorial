ef1a0e882ca9d54b9a37d8dfa3120002df201f89
Suemon-Kwok <149294826+Suemon-Kwok@users.noreply.github.com>
2026-07-05 13:40:46 +1200

    Implement basic arithmetic operations in Arithmetic class
    Updated the Arithmetic class to include basic arithmetic operations. The main method now declares two integers, prints their values, and calculates their sum, product, and modulus.

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,34 +1,45 @@
-/*Object oriented programming Lab 1 question 4
+/*Object oriented programming Lab 1 question 3
 
-Complete the main method in the CircleAreaCalculator class to print the formatted output value of area
+Complete the main method in the Arithmetic class by
+1.	Declaring two integer-typed variables x = 5, and y = 3
+2.	Printing the formatted output of x and y on separate lines on the console
+3.	Declaring three integer-typed variables sum = x + y, product = x * y and mod = x % y
+4.	Printing the formatted output of sum, product and mod on separate lines on the console
 For example:
 Test	Result
-CircleAreaCalculator.main(new String[]{})
+Arithmetic.main(new String[]{})	x = 5
+y = 3
+sum = 8
+product = 15
+mod = 2
 
-	area = 346.36059005827474
-
-
-
-public class CircleAreaCalculator 
+public class Arithmetic 
 {
-	public static void main(String[] args)
-	{
-		double radius = 10.5;
-		double area = Math.PI * radius * radius;
-		System.out.println(“   ”);	
-	}
+
+    public static void main(String[] args) 
+    {
+
+    
+    
+    }
 }
-basic mathematical operations
+basic arithmetic operations and integer manipulation
 */
 
 
-
-public class CircleAreaCalculator 
+public class Arithmetic 
 {
-	public static void main(String[] args)
-	{
-		double radius = 10.5;
-		double area = Math.PI * radius * radius;
-		System.out.println("area = " + area);	
-	}
+public static void main(String[] args)
+{
+int x = 5;
+int y =3;
+System.out.println("x = "+x);
+System.out.println("y = "+y);
+int sum = x + y;
+int product = x * y;
+int mod = x % y;
+System.out.println("sum = " + sum);
+System.out.println("product = " + product);
+System.out.println("mod = " +mod);
+}
 }
